a242bbabfc7c0eb8f5b48608a3fdf8b3f5f6ce26
Isuru Sampath Ratnayake <isuru.sampath@ratnayake.info>
2012-09-01 13:32:15 +0530

    spell check

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ include "./config.php";
 include "46elks/46elks.php";
 
 $A6elka = new A6elks($A6elks_username,$A6elks_password);
-//allocate sweeden sms only phone number
+//allocate sweden sms only phone number
 $response = $A6elka->Allocate_Number('se','http://mydpmain.com/sms/index.php');
 
